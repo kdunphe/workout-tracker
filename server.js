6940dcc99db60f5af111e96386b0333650375381
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-11-20 10:50:01 -0500

    Adding MongoDB connection

--- server.js
+++ server.js
@@ -4,23 +4,32 @@ const mongoose = require('mongoose');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-require('dotenv').config()
-
 // Set Up Express
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Set Up App
 app.use(logger('dev'));
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // API Routes
 app.use(apiRoutes);
 
 // HTML Routes
 app.use(htmlRoutes);
+
+// Database Connection
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 // Start the Server
 app.listen(PORT, () => {
