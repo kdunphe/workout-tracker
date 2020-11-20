3be618c388fd6b4f866900cb7f989102c910b259
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-11-20 13:02:20 -0500

    Fixing API routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,34 +1,54 @@
 const apiRoutes = require("express").Router();
-const db = require("../models/workout");
+const db = require("../models");
 
-apiRoutes.get("/api/workouts", (req, res) => {
-  db.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+apiRoutes.get("/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(workout => {
+            res.json(workout);
+        }).catch((err) => {
+            res.json(err);
+        });
 });
 
-apiRoutes.get("/api/workouts/range", (req, res) => {
-  db.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+apiRoutes.get("/workouts/range", (req, res) => {
+    db.Workout.find({})
+        .then(workout => {
+            res.json(workout);
+        }).catch((err) => {
+            res.json(err);
+        });
 });
 
-apiRoutes.post("/api/workouts", (req, res) => {
-  db.create({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+apiRoutes.post("/workouts", (req, res) => {
+    db.Workout.create({})
+        .then(workout => {
+            res.json(workout);
+        }).catch(err => {
+            res.json(err);
+        });
+});
+
+apiRoutes.post("/workouts/range", (req, res) => {
+    db.Workout.create({})
+        .then(workout => {
+            res.json(workout);
+        }).catch(err => {
+            res.json(err);
+        })
+})
+
+apiRoutes.put("/workouts/:id", ({ body, params }, res) => {
+    db.Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        { new: true }
+    )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
 
 module.exports = apiRoutes;
