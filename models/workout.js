37b3d4611121bc52f97057aec4fce6846320f5c5
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-11-20 12:49:41 -0500

    Updating model for workouts

--- models/workout.js
+++ models/workout.js
@@ -3,49 +3,43 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
 
-  day: {
-    type: Date,
-    default: Date.now
-  },
+    day: {
+        type: Date,
+        default: Date.now
+    },
 
-  exercises: [
-    {
-      type: {
-        type: String,
-        trim: true,
-        required: "Enter an exercise type."
-      },
-      name: {
-        type: String,
-        trim: true,
-        required: "Enter an exercise name."
-      },
-      distance: {
-        type: Number
-      },
-      duration: {
-        type: Number,
-        required: "Enter the duration."
-      },
-      weight: {
-        type: Number
-      },
-      reps: {
-        type: Number
-      },
-      sets: {
-        type: Number
-      }
-    }
-  ]
-},
-{
-  toJSON:
-  {
-    virtuals: true
-  }
-}
-);
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter an exercise type."
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter an exercise name."
+            },
+            distance: {
+              type: Number
+            },
+            duration: {
+                type: Number,
+                trim: true,
+                required: "Enter the duration."
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+              type: Number
+            },
+            sets: {
+                type: Number
+            }
+        }
+    ]
+});
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
